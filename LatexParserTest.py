4b0c881bbee7b7d1629515b29bfa5376c2847082
Emmanuel Ariza <ejariza@gmail.com>
2015-03-26 12:30:35 -0500

    Initial commit

---
+++ LatexParserTest.py
@@ -0,0 +1,11 @@
+import unittest
+from LatexParser import LatexParser
+
+class LatexParserTest(unittest.TestCase):
+
+    def test_LatexParserExist(self):
+        latexParser = LatexParser()
+
+
+if __name__ == "__main__":
+    unittest.main()
